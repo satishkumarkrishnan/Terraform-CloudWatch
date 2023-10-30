350047cfc1e08f81c854268301bb5fc423645c6f
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-30 20:17:41 +0530

    Added new o/p variable

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "cloudwatch" {
 resource "aws_cloudwatch_log_group" "tokyo_cw" {
   name = "tokyo_cw"  
   retention_in_days = "7"
-  kms_key_id = module.cloudwatch.kms_arn
+  kms_key_id = module.cloudwatch.aws_kms_key_policy.tokyo_kms_key_policy.id
   tags = {
     Environment = "dev"
     Application = "helloworld"
