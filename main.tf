48ec5199c4e636f322537b964e67d0c27b4b21da
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-30 12:38:04 +0530

    Added KMS Key to CW log with retention extension

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
 #Module code for importing KMS code
 module "cloudwatch" {
   source ="git@github.com:satishkumarkrishnan/Terraform-KMS.git?ref=main"
+  depends_on = [module.kms_key_id.arn]
 }
 
 #TF code for AWS CW Log Group Creation
