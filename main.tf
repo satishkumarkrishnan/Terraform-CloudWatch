57c663e9f7747c8a8add44675ad65a46582d4169
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-30 12:39:59 +0530

    Added KMS Key based on depends on condition

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 #Module code for importing KMS code
 module "cloudwatch" {
   source ="git@github.com:satishkumarkrishnan/Terraform-KMS.git?ref=main"
-  depends_on = [module.kms_key_id.arn]
+  depends_on = [module.kms.tok]
 }
 
 #TF code for AWS CW Log Group Creation
