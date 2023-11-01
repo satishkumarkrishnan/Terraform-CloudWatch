8d335c7e139d4407011c161adeb2b9315a7a0648
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-01 09:52:48 +0530

    commented insuffucient data

--- cw_metric_alarm.tf
+++ cw_metric_alarm.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_metric_alarm" "tokyo_cw_ma" {
   alarm_name                = "tokyo-cw-ma"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 1
+  evaluation_periods        = 2
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/EC2"
-  period                    = 10
+  period                    = "120"
   statistic                 = "Average"
-  threshold                 = 1
+  threshold                 = "80"
   alarm_description         = "This metric monitors ec2 cpu utilization"
   treat_missing_data        = "missing"
   #insufficient_data_actions = []
