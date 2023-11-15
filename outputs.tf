8a00af27a7cded4560bb921825ab4782d077645f
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-15 13:23:53 +0530

    commented instnace in o/p.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
-output "asg_ec2" {
-  value = module.asg.instance.id  
-}
+/*output "asg_ec2" {
+  #value = module.asg.instance.id
+  value = module.asg.instance.id
+}*/
 
 output "cw_log_group" {
   value = aws_cloudwatch_log_group.tokyo_cw.name 
