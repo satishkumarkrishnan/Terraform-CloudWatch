4ddc0126dda2d12d0ba6800c30ae273e65650e99
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-16 11:02:57 +0530

    S3 version disabled

--- s3.tf
+++ s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "kms_encrypted" {
 resource "aws_s3_bucket_versioning" "enabled" {
   bucket = aws_s3_bucket.kms_encrypted.id  
   versioning_configuration {
-    status = "Disabled"
+    status = "Enabled"
   }
 }
 # Enable server-side encryption by default
