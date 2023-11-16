eb019c0e7c4d831e5dc40a015aadba2e9b93e366
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-16 09:42:31 +0530

    added o/p.tf

--- outputs.tf
+++ outputs.tf
@@ -12,27 +12,7 @@ output "s3_bucket" {
   value = aws_s3_bucket.kms_encrypted.bucket
 }
 
-output "vpc_id" {
-  value = aws_vpc.tokyo-vpc.id
-}
-
-output "vpc_fe_subnet" {
-  value = aws_subnet.private[0]
-}
-
-output "vpc_be_subnet" {
-  value = aws_subnet.private[1]
-}
-
-output "vpc_fe_sg" {
-  value = aws_security_group.tokyo-securitygroup[0].id
-}
-
-output "vpc_be_sg" {
-  value = aws_security_group.tokyo-securitygroup[1].id
-}
-
-output "kms_arn" {
+output "kms_id" {
+  #value = aws_s3_bucket.kms_encrypted.name
   value = module.kms.kms_arn
 }
-
